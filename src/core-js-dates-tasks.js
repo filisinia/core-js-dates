c6d4ce1569db7dc8ab03a600a41081d6c5b8ada5
Mariia Petrenko <filisinia@gmail.com>
2024-03-25 14:13:50 +0200

    feat: solve isDateInPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -118,8 +118,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+  const dateToCheck = new Date(date);
+
+  return dateToCheck >= start && dateToCheck <= end;
 }
 
 /**
